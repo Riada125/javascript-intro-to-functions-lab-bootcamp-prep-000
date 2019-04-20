0247ce3f2a8573dfe40ebc63d8eff6bba0e7d874
Michael Adair <riada125@hotmail.com>
2019-04-20 13:06:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,11 +11,14 @@ function logWhisper() {
    console.log("hello")
 }
 
-function sayHiToGrandma(string) {
- if (string.toLowerCase() === string) 
- console.log(“I can’t hear you!”);
- if (string.toUpperCase() === string )
- console.log(“YES,INDEED!”)
- if (string === `I love you, Grandma.` ) 
- console.log(“I love you, too.”)
-}
+function sayHiToGrandma(string){
+  var uppercase = "HELLO"
+  var lowercase = "hello"
+  if (string === lowercase){
+  if (string === string.toLowerCase()){
+    return "I can't hear you!"
+  }else if (string === uppercase){
+  }else if (string === string.toUpperCase()){
+    return "YES INDEED!"
+  }else if (string === "I love you, Grandma."){
+    return "I love you, too."
