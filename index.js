0dcaf5324baa6a005134a83a093fccd9414f1dbb
Michael Adair <riada125@hotmail.com>
2019-04-20 12:31:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function logWhisper() {
    console.log("hello")
 }
 function sayHiToGrandma(string) {
-  control.log("hello".toLowerCase)
+  console.log("hello".toLowerCase)
   return("I can't hear you!")
 }
