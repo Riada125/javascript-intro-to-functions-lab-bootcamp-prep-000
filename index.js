7fb9e34ff3d2d10b4ea1b60c5c8782e2281f8e70
Michael Adair <riada125@hotmail.com>
2019-04-20 11:36:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function whisper(string) {
   return string.toLowerCase()
 }
 function logShout(string) {
-   console.log($string)
-   logShout ("HELLO")
+   console.log(${string})
+   logShout ("HELLO");
 }
